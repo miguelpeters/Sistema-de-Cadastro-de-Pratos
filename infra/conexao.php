f7ca5be7bb533509cc80a3d6a7d81eaad2ac4433
theo-maccari <theo_maccari@estudante.sesisenai.org.br>
2026-08-19 09:28:21 -0300

    up - conexão atualizada

--- infra/conexao.php
+++ infra/conexao.php
@@ -1,14 +1,14 @@
 <?php
 
 $host = "localhost";
-$usuario = "root.";
-$senha = "root";
+$usuario = "root";
+$senha = "";
 $banco = "prato_db";
 
 $conexao = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conexao->connect_error) {
-    die("Erro na conexão com o banco: " . $conexao->connect_error);
+    die("Erro na conexão: " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8mb4");
